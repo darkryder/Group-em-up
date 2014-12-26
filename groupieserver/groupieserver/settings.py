c5273791e2a5535f7f23f2819e3dac1a68d6f6c1
darkryder <sambhav13085@iiitd.ac.in>
2014-12-26 23:02:27 +0530

    hopefully allow cors requests

--- groupieserver/groupieserver/settings.py
+++ groupieserver/groupieserver/settings.py
@@ -37,16 +37,19 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'server',
+    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'groupieserver.urls'
 
